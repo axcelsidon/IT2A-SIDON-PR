4efdb5b4bbda696fcf94906b50886c091e6e4a44
Axcelsidon <Axcelsidon>
2024-11-18 14:44:11 +0800

    Update

---
+++ NetBeansProjects/Property_Rental2A/src/property_rental2a/Cofig.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package property_rental2a;
+
+/**
+ *
+ * @author milan
+ */
+public class Cofig {
+    
+}
